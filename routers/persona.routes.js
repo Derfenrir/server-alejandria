f185442bdd4a2ebf10e056c9a6e3ada7b658f082
alejandro <alejandrosr543@gmail.com>
2022-07-06 17:45:38 -0500

    actualizacion 2.0

--- routers/persona.routes.js
+++
@@ -1,8 +0,0 @@
-import routesx from 'express-promise-router';
-import personasController from '../controllers/personas.controller';
-
-const router=routesx();
-
-router.post('/guardar',personasController.addPersona);
-
-export default router;
